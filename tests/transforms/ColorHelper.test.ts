f696361df3252f6d6dc4a53ddb305fae666f8fcf
Jiri Trecak <jiritrecak@gmail.com>
2023-09-28 21:13:42 -0700

    Add color value / reference formatter and tests

--- tests/transforms/ColorHelper.test.ts
+++ tests/transforms/ColorHelper.test.ts
@@ -1,4 +1,4 @@
-import { ColorTokenValue, Unit } from "@supernova-studio/pulsar-next"
+import { ColorToken, ColorTokenValue, OpacityToken, Token, Unit } from "@supernova-studio/pulsar-next"
 import { ColorFormat, ColorHelper, NamingHelper, StringCase } from "../../src/exports"
 
 const testDecimals = 3
@@ -16,6 +16,15 @@ const testColor: ColorTokenValue = {
   },
   referencedTokenId: null,
 }
+const tokens = new Map<string, Token>()
+tokens.set("colorRef", {
+  value: testColor,
+  name: "colorRef",
+} as ColorToken)
+tokens.set("opacityRef", {
+  value: testColor.opacity,
+  name: "opacityRef",
+} as OpacityToken)
 
 test("formattedColor_hex6", () => {
   expect(ColorHelper.formattedColor(testColor, ColorFormat.hex6, testDecimals)).toBe("8764c8")
@@ -73,4 +82,147 @@ test("formattedColor_smartUIColor", () => {
   expect(ColorHelper.formattedColor(testColor, ColorFormat.smartUIColor, testDecimals)).toBe(
     "UIColor(rgb: 0x8764c8).withAlphaComponent(0.5)"
   )
+})
+
+test("formattedColorOrVariableName_raw", () => {
+  const color: ColorTokenValue = {
+    color: {
+      r: 135,
+      g: 100,
+      b: 200,
+      referencedTokenId: null,
+    },
+    opacity: {
+      measure: 0.5,
+      referencedTokenId: null,
+      unit: Unit.raw,
+    },
+    referencedTokenId: null,
+  }
+
+  expect(
+    ColorHelper.formattedColorOrVariableName(
+      color,
+      tokens,
+      ColorFormat.smartRgba,
+      testDecimals,
+      (colorToken) => {
+        return `var(--${colorToken.name})`
+      },
+      (opacityToken) => {
+        return `var(--${opacityToken.name})`
+      }
+    )
+  ).toBe("rgba(135, 100, 200, 0.5)")
+})
+
+test("formattedColorOrVariableName_fullReference", () => {
+  const color: ColorTokenValue = {
+    color: {
+      r: 135,
+      g: 100,
+      b: 200,
+      referencedTokenId: null,
+    },
+    opacity: {
+      measure: 0.5,
+      referencedTokenId: null,
+      unit: Unit.raw,
+    },
+    referencedTokenId: "colorRef",
+  }
+
+  expect(
+    ColorHelper.formattedColorOrVariableName(
+      color,
+      tokens,
+      ColorFormat.smartRgba,
+      testDecimals,
+      (colorToken) => {
+        return `var(--${colorToken.name})`
+      },
+      (opacityToken) => {
+        return `var(--${opacityToken.name})`
+      }
+    )
+  ).toBe("var(--colorRef)")
+})
+
+test("formattedColorOrVariableName_partialColorReference", () => {
+  const color: ColorTokenValue = {
+    ...testColor,
+    color: {
+      ...testColor.color,
+      referencedTokenId: "colorRef",
+    },
+  }
+
+  expect(
+    ColorHelper.formattedColorOrVariableName(
+      color,
+      tokens,
+      ColorFormat.smartRgba,
+      testDecimals,
+      (colorToken) => {
+        return `var(--${colorToken.name})`
+      },
+      (opacityToken) => {
+        return `var(--${opacityToken.name})`
+      }
+    )
+  ).toBe("rgba(var(--colorRef), 0.5)")
+})
+
+test("formattedColorOrVariableName_partialOpacityReference", () => {
+  const color: ColorTokenValue = {
+    ...testColor,
+    opacity: {
+      ...testColor.opacity,
+      referencedTokenId: "opacityRef",
+    },
+  }
+
+  expect(
+    ColorHelper.formattedColorOrVariableName(
+      color,
+      tokens,
+      ColorFormat.smartRgba,
+      testDecimals,
+      (colorToken) => {
+        return `var(--${colorToken.name})`
+      },
+      (opacityToken) => {
+        return `var(--${opacityToken.name})`
+      }
+    )
+  ).toBe("rgba(135, 100, 200, var(--opacityRef))")
+})
+
+test("formattedColorOrVariableName_partialBothReferences", () => {
+  const color: ColorTokenValue = {
+    ...testColor,
+    color: {
+      ...testColor.color,
+      referencedTokenId: "colorRef",
+    },
+    opacity: {
+      ...testColor.opacity,
+      referencedTokenId: "opacityRef",
+    },
+  }
+
+  expect(
+    ColorHelper.formattedColorOrVariableName(
+      color,
+      tokens,
+      ColorFormat.smartRgba,
+      testDecimals,
+      (colorToken) => {
+        return `var(--${colorToken.name})`
+      },
+      (opacityToken) => {
+        return `var(--${opacityToken.name})`
+      }
+    )
+  ).toBe("rgba(var(--colorRef), var(--opacityRef))")
 })
